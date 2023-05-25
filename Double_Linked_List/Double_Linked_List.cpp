9cfac50e0aa3c1bbcab58eb6e7cad0f23e420d04
windyaan <windyanurmaretha@gmail.com>
2023-05-25 11:07:04 +0700

    Traverse untuk memasukkan data

--- Double_Linked_List/Double_Linked_List.cpp
+++ Double_Linked_List/Double_Linked_List.cpp
@@ -54,4 +54,9 @@ void DoubleLinkedList::addNode() {
 		START = newNode;
 		return;
 	}
+
+	Node* current = START;
+	while (current->next != NULL && nim > current->next->noMhs)
+		current = current->next;
+
 }
