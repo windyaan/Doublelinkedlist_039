80351358e44c612ca49d01ed772f06b99199409d
windyaan <windyanurmaretha@gmail.com>
2023-05-25 11:10:59 +0700

    Jika data yang ditambahkan telah ada

--- Double_Linked_List/Double_Linked_List.cpp
+++ Double_Linked_List/Double_Linked_List.cpp
@@ -59,4 +59,9 @@ void DoubleLinkedList::addNode() {
 	while (current->next != NULL && nim > current->next->noMhs)
 		current = current->next;
 
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll numbers not allowed" << endl;
+		return;
+	}
+
 }
