427a4469ceafbe81ebc411b8fd5c0b14810b613d
windyaan <windyanurmaretha@gmail.com>
2023-05-25 11:18:44 +0700

    Jika var current menunjuk kosong, maka newNode diletakkan diakhir

--- Double_Linked_List/Double_Linked_List.cpp
+++ Double_Linked_List/Double_Linked_List.cpp
@@ -66,5 +66,7 @@ void DoubleLinkedList::addNode() {
 
 	newNode->next = current->next;
 	newNode->prev = current;
-
+	if (current->next != NULL)
+		current->next->prev = newNode;
+	current->next = newNode;
 }
